c4514a110a0a127d207eda660810739982dc4787
Chiamh <Chiamh@users.noreply.github.com>
2015-03-19 00:21:39 +0100

    moving tcx specific files to sub package - part 2

--- common/src/main/java/de/npo/trainingcenter/common/tcx/TcxTrackLogic.java
+++ common/src/main/java/de/npo/trainingcenter/common/tcx/TcxTrackLogic.java
@@ -1,6 +1,9 @@
-package de.npo.trainingcenter.common;
+package de.npo.trainingcenter.common.tcx;
 
 import java.util.List;
+
+import de.npo.trainingcenter.common.TrackLap;
+import de.npo.trainingcenter.common.TrackLogic;
 
 public interface TcxTrackLogic extends TrackLogic {
 
